321bf29f7e5ea03068a10c132ce1171b0421789a
wlodzimierrr <wlodzimierrr@github.com>
2023-10-03 15:52:24 +0100

    random message loop and switch for the personal message generator

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function generateRandomNumber(num) {
     return Math.floor(Math.random() * num)
   };
 
-  // storage for messages
+// storage for messages
 const messageStorage = {
     advice: [],
     fortunateOutput:[],
@@ -12,3 +12,24 @@ const messageStorage = {
 
 // personal message array
 let personalMessage = [];
+
+// random message loop
+for(let prop in messageStorage) {
+    let option = generateRandomNumber(messageStorage[prop].length)
+
+// The first part of a message and the message from the object added to create personal message  
+    switch(prop) {
+      case 'advice':
+        messageStorage.push(`You should: "${messageStorage[prop][option]}".`)
+        break
+      case 'fortunateOutput':
+        messageStorage.push(`You are having: "${messageStorage[prop][option]}".`)
+        break
+      case 'whoYouAre':
+        messageStorage.push(`You are: "${messageStorage[prop][option]}".`)
+        break
+      default:
+        messageStorage.push('There is not enough info.')
+        
+    };
+};
